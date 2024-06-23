1c4f374a7e19b9ea8ffec5bf3e73d5ae9b7a344e
nitesh <niteshmonangi@gmail.com>
2024-06-23 20:16:39 +0530

    task 5

---
+++ task 7.py
@@ -0,0 +1,17 @@
+
+
+name = input("enter the name")
+b =name[:: -1]
+print (b)
+
+def my_inner(x):
+  return x[:: -1]
+
+l = my_inner("the royal life of world")
+print (l)
+
+t = ['sddi', 'ram','roy']
+f = t[:: -1]
+print(f)
+
+
---
+++ task 8.py
@@ -0,0 +1,14 @@
+name = "nitesh"
+
+vowels = 0
+consonants = 0
+
+for i in name:
+    if (i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u'
+            or i == 'A' or i == 'E' or i == 'I' or i == 'O' or i == 'U'):
+        vowels = vowels + 1
+    else:
+        consonants = consonants + 1
+
+print("Total Number of Vowels in this String = ", vowels)
+print("Total Number of Consonants in this String = ", consonants)
